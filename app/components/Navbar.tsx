356330c0251ea859b9be62a863a790420792612e
alenai <helpsmolpuppy@gmail.com>
2024-02-08 21:31:42 +0300

    fix navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Search from "./Search";
 
 export default function Navbar() {
   return (
-    <nav className="bg-slate-600 p-4 flex justify-between flex-col md:flex-row sticky top-0 drop-shadow-xl">
+    <nav className="bg-slate-600 p-4 flex justify-center gap-x-10 flex-col md:flex-row sticky top-0 drop-shadow-xl">
       <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
         <Link href="/">WikiFind!</Link>
       </h1>
